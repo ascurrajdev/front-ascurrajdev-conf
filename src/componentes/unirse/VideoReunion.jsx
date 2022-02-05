c21e4608a3a0a86932d027dee42a3e308cdb047b
Jose Ascurra <info@ascurrajdev.xyz>
2022-02-05 14:28:00 -0300

    Cambios en el control de video para mostrar las videollamadas

--- src/componentes/unirse/VideoReunion.jsx
+++ src/componentes/unirse/VideoReunion.jsx
@@ -6,7 +6,7 @@ const VideoReunion = ({peerMedia,muted}) => {
     },[])
     return(
         <div className="col">
-            <video ref={videoMedia} controls={false} autoPlay={true} muted={muted} className="bg-black w-100"></video>
+            <video ref={videoMedia} playsinline controls={false} autoPlay={true} muted={muted} className="bg-black w-100" />
         </div>
     )
 }
